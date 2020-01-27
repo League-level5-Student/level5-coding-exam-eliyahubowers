538886544242af70de8abf1371b940fa21eadbc3
League Student <league@iMac-8.attlocal.net>
2020-01-26 16:00:54 -0800

    Update CodingExamA.java

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -21,6 +21,60 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
-
+		
+		String s = JOptionPane.showInputDialog("How many Robots");
+		
+		Robot[] r = new Robot[Integer.parseInt(s)];
+		
+		int increment = 0;
+		
+		for(int i = 0 ; i < r.length; i++) {
+			r[i] = new Robot();
+			r[i].penDown();
+			r[i].setX((i-(12*increment))*200);
+			r[i].setY(increment*200);
+			if(increment%12 == 0) {
+				increment++;
+			}
+		}
+		
+		s = JOptionPane.showInputDialog("What Color: ( red , green , blue)");
+		
+		switch (s) {
+		case "red":
+			for(int i = 0 ; i < r.length; i++) {
+				r[i].setPenColor(Color.red);
+			}
+			break;
+		case "green":
+			for(int i = 0 ; i < r.length; i++) {
+				r[i].setPenColor(Color.green);
+			}
+			break;
+		case "blue":
+			for(int i = 0 ; i < r.length; i++) {
+				r[i].setPenColor(Color.blue);
+			}
+			break;
+		}
+		
+		s = JOptionPane.showInputDialog("How many sides to a shape? ( >0 )");
+		
+		int sides = Integer.parseInt(s);
+		int angle = 360/sides;
+		
+		Thread[] t = new Thread[r.length];
+		
+		for(int i = 0; i < r.length; i++) {
+			t[i] = new Thread(() -> {
+				for(int j = 0; j < sides; j++) {
+					r[i].move(30);
+					r[i].turn(angle);
+				}
+			});
+		}
+		
+		
+		
 	}
 }
